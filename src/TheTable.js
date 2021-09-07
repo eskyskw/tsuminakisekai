e42cc05678c4c6a411079ae1bcebba4edcdb35c7
eskyskw <esk.yskw@gmail.com>
2021-09-07 21:32:49 +0900

    入力ができるよう修正

--- src/TheTable.js
+++ src/TheTable.js
@@ -20,39 +20,21 @@ export default function TheTable( {data=[]} ) {
             accessor: 'price'
         }
     ];
-    const {
-      getTableProps,
-      getTableBodyProps,
-      headerGroups,
-      rows,
-      prepareRow,
-    } = useTable({
-      columns,
-      data,
-    });
   
     return (
-      <table {...getTableProps()}>
+      <table>
         <thead>
-          {headerGroups.map(headerGroup => (
-            <tr {...headerGroup.getHeaderGroupProps()}>
-              {headerGroup.headers.map(column => (
-                <th {...column.getHeaderProps()}>{column.render('Header')}</th>
-              ))}
+          {columns.map(c => (<td> {c.Header} </td>))}
+        </thead>
+        <tbody>
+          {data.map(d => (
+            <tr>
+              {columns.map(c => {
+                const acc = c.accessor;
+                return (<td> {d[acc]} </td>);
+              })}
             </tr>
           ))}
-        </thead>
-        <tbody {...getTableBodyProps()}>
-          {rows.map((row, i) => {
-            prepareRow(row)
-            return (
-              <tr {...row.getRowProps()}>
-                {row.cells.map(cell => {
-                  return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
-                })}
-              </tr>
-            );
-          })}
         </tbody>
       </table>
     );
